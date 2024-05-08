9bb103fa7566124ab55ccccc15fcfe9ce5a7d4d3
Felipe <166537958+feliperaviolo@users.noreply.github.com>
2024-05-08 11:00:19 -0300

    Update main.py

--- PDF/main.py
+++ PDF/main.py
@@ -9,16 +9,13 @@ from openpyxl import load_workbook
 def extraccion_info(pdf_file_path):
     #abrir el PDF file
     with open(pdf_file_path,'rb') as file:
-
         pdf_render = PyPDF2.PdfReader(file)
-
         text =''
-
         for page_num in range(len(pdf_render.pages)):
             page = pdf_render.pages[page_num]
             text += page.extract_text()
 
-        #Extresiones Regulares
+        #Extresiones Regulares (cambiar para buscar en las facturas los datos que se quieren encontrar)
         numero_f = r"Nro:\s*(\d+-\d+)"
         items_e= r"Calibración \s*(.*)"
         Importe_f = r"Total\s*(.*)"
@@ -35,10 +32,6 @@ def extraccion_info(pdf_file_path):
         Importe= import_match.group(1) if import_match else None
         Proveedor= proveedor_match.group(1) if proveedor_match else None
 
-        
-
-        
-
         return numero, item, Importe, Proveedor
     
 def get_files_in_folder(folder_path):
@@ -49,7 +42,7 @@ def get_files_in_folder(folder_path):
     return files
 
 if __name__ == "__main__":
-    folder_path = "Facturas"
+    folder_path = "Facturas" #Colocar el nombre de la carpeta donde se localizan las facturas que no fueron Procesada
     files= get_files_in_folder(folder_path)
 
     for file in files:
@@ -68,14 +61,14 @@ if __name__ == "__main__":
             "Importe":[Importe],
             "Proveedor":[Proveedor]
         }
-        archivo_excel = 'Libro2.xlsx'
+        archivo_excel = 'Libro2.xlsx' #Colocar el nombre del Excel
         df_existente = pd.read_excel(archivo_excel)
         nuevo_df = pd.DataFrame(data, index=[0])
         df_actualizado = pd.concat([df_existente, nuevo_df], ignore_index=True)
         df_actualizado.to_excel(archivo_excel, index=False)
 
         #mover a la carpeta de vista
-        realizado_file = "Faturas_vista"
+        realizado_file = "Faturas_vista" #Colocar nombre de la carpetaa donde van las facturas que fueron Procesadas
         os.makedirs(realizado_file, exist_ok=True)
         new_files_path= os.path.join(realizado_file, os.path.basename(file))
         os.rename(file,new_files_path)
